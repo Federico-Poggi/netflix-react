94890e0b02170889fbaf064570b373704f0e7cfe
Federico Poggi <federicopoggi3@gmail.com>
2023-11-02 16:48:32 +0100

    added_link_tonav

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,7 +1,11 @@
 import { Navbar, Nav } from "react-bootstrap";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const MyNavbar = () => {
+  const location = useLocation();
+
+  console.log("OGGETTO LOCATION", location);
+
   return (
     <Navbar variant="dark" expand="lg" style={{ backgroundColor: "#221f1f" }}>
       <Navbar.Brand href="/">
@@ -14,14 +18,57 @@ const MyNavbar = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link className="font-weight-bold" href="/">
+          {/* <Nav.Link className="font-weight-bold" href="/">
             Home
-          </Nav.Link>
+          </Nav.Link> */}
+          <Link
+            className={
+              location.pathname === "/home" ? "nav-link active" : "nav-link"
+            }
+            to="/home"
+          >
+            Home
+          </Link>
 
-          <Nav.Link active className="font-weight-bold" href="#tvshow">
+          {/* <Link active className="font-weight-bold">
             TV Shows
-          </Nav.Link>
-          <Nav.Link className="font-weight-bold" href="/">
+          </Link> */}
+          <Link
+            className={
+              location.pathname === "/tvshow" ? "nav-link active" : "nav-link"
+            }
+            to="/tvshow"
+          >
+            Tv Show
+          </Link>
+
+          <Link
+            className={
+              location.pathname === "/movies" ? "nav-link active" : "nav-link"
+            }
+            to="/movies"
+          >
+            Movies
+          </Link>
+          <Link
+            className={
+              location.pathname === "/recently_added"
+                ? "nav-link active"
+                : "nav-link"
+            }
+            to="/recently_added"
+          >
+            Recently Added
+          </Link>
+          <Link
+            className={
+              location.pathname === "/mylist" ? "nav-link active" : "nav-link"
+            }
+            to="/mylist"
+          >
+            My List
+          </Link>
+          {/* <Nav.Link className="font-weight-bold" href="/">
             Movies
           </Nav.Link>
           <Nav.Link className="font-weight-bold" href="/">
@@ -29,7 +76,7 @@ const MyNavbar = () => {
           </Nav.Link>
           <Nav.Link className="font-weight-bold" href="/">
             My List
-          </Nav.Link>
+          </Nav.Link> */}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
